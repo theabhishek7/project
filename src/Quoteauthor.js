886a112d91651b72d16f7b652b9b0a22e3d4ec20
Abhishek <krabhi745@gmail.com>
2020-12-05 10:50:05 +0530

    first commit

---
+++ src/Quoteauthor.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class QuoteAndAuthor extends React.Component {
+    render() {
+        const randomColor = this.props.displayColor();
+        const html = document.documentElement;
+        html.style.backgroundColor = randomColor;
+
+        return ( <
+            div style = {
+                { backgroundColor: "green" }
+            }
+            className = "quotebox" >
+            <
+            div key = { Math.random() }
+            style = {
+                { color: randomColor }
+            } >
+            <
+            h1 id = "quote" > "{this.props.quote}" < /h1> <
+            h5 id = "author" >
+            -{ this.props.author ? this.props.author : "Unknown" } -
+            <
+            /h5> < /
+            div > <
+            button style = {
+                { backgroundcolor: randomColor }
+            }
+            id = "newquote"
+            onClick = { this.props.handleClick } >
+            New quote <
+            /button> < /
+            div >
+        );
+    }
+}
+
+export default QuoteAndAuthor;
---
+++ src/QuotesDatabase.js
@@ -0,0 +1,17 @@
+const quotes = [{
+    "quote": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    "author": "Thomas Edison"
+}, {
+    "quote": "You can observe a lot just by watching.",
+    "author": "Yogi Berra"
+}, {
+    "quote": "A house divided against itself cannot stand.",
+    "author": "Abraham Lincoln"
+}, {
+    "quote": "Only do what your heart tells you.",
+    "author": "Princess Diana"
+}, {
+    "quote": "Life is movement-we breathe, we eat, we walk, we move!",
+    "author": "John Pierrakos"
+}]
+export default quotes
